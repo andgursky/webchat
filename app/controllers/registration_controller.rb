72ac57309b035f3abe25826e38d920a65a2bc056
andgursky <andgursky@gmail.com>
2016-09-23 00:40:50 +0300

    RegistrationController - specs covering

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,14 +1,16 @@
 class RegistrationController < ApplicationController
   def registration
-
   end
 
   def registration_handler
-    client_name=params['user_name']
-    client_login=params['user_login']
-    client_password=params['user_password']
+    client_name = params['user_name']
+    client_login = params['user_login']
+    client_password = params['user_password']
+
     if(Client.find_by_login(client_login)==nil)
-      u=Client.new(:name => client_name,:login=>client_login,:password =>client_password)
+      u = Client.new(:name=>client_name,
+                     :login=>client_login,
+                     :password=>client_password)
       u.save
       redirect_to "/"
     end
---
+++ spec/controllers/registration_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe RegistrationController, type: :controller do
+  describe "POST #registration_handler" do
+    render_views
+
+    describe "when we have new client" do
+      reg_params = { :user_name=>"new_name", :user_login=>"new_login",
+                     :user_password=>"new_password" }
+
+      it "redirects to '/' if client doesn't exist in 'Clients' db" do
+        post :registration_handler, reg_params
+        expect(response).to redirect_to("/")
+      end
+
+      it "save new user if client doesn't exist" do
+        expect_any_instance_of(Client).to receive(:save).and_call_original
+        post :registration_handler, reg_params
+      end
+    end
+
+    describe "when the old client trying to register with his credentials" do
+      client = FactoryGirl.create(:client, name: "John", login: "John",
+                                  password: "111")
+      reg_params = { :user_name=>client.name,
+                     :user_login=>client.login,
+                     :user_password=>client.password }
+
+      it "shows 'Login incorrect' on the /registration_handler page" do
+        post :registration_handler, reg_params
+        expect(response.body).to include("Login incorrect")
+      end
+
+      it "renders /registration_handler page" do
+        post :registration_handler, reg_params
+        expect(response).to render_template("registration_handler")
+      end
+    end
+  end
+end
