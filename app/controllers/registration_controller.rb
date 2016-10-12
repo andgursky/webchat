f9480f3eaf178888e7e46732e22b1ca783cbe446
andgursky <andgursky@gmail.com>
2016-10-13 02:30:54 +0300

    Registration - fix bug with params indexes

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,9 +3,9 @@ class RegistrationController < ApplicationController
   end
 
   def registration_handler
-    client_name = params['user_name']
-    client_login = params['user_login']
-    client_password = params['user_password']
+    client_name = params["name"]
+    client_login = params["login"]
+    client_password = params["password"]
 
     if(Client.find_by_login(client_login)==nil)
       u = Client.new(:name=>client_name,
